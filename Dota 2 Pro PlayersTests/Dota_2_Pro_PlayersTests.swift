0096b01faad575d18822684dc48e96968ae0fd29
Jora <jora@joras-macbook-pro.local>
2018-09-28 11:02:59 +0300

    Initial Commit

---
+++ Dota 2 Pro PlayersTests/Dota_2_Pro_PlayersTests.swift
@@ -0,0 +1,34 @@
+//
+//  Dota_2_Pro_PlayersTests.swift
+//  Dota 2 Pro PlayersTests
+//
+//  Created by Jora on 9/28/18.
+//  Copyright © 2018 Jora. All rights reserved.
+//
+
+import XCTest
+@testable import Dota_2_Pro_Players
+
+class Dota_2_Pro_PlayersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
